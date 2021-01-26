11de66b2f0990e515a97e82c96db4de48db84a6f
Lee JaeHyun <myleje@gmail.com>
2021-01-26 10:41:21 +0900

    .

--- Program.cs
+++ Program.cs
@@ -11,17 +11,11 @@ namespace CSharpBasic2
     */
     public class Enemy
     {
-        //멤버 - 필드(Field) : OOP 인닉성 특징
-        private string name;
         private int hp = 100;
         private int speed = 10;
 
         //프로퍼티(Property) : 외부에 노출되는 속성(변수)
-        public string Name
-        {
-            get {return this.name;}
-            set {this.name = value;}
-        }
+        public string Name { get; set; }
 
         public int Hp
         {
@@ -31,10 +25,28 @@ namespace CSharpBasic2
                 
                 if (this.hp <= 0)
                 {
-                    Console.WriteLine($"{this.name} is died!");
+                    Console.WriteLine($"{this.Name} is died!");
                 }
             }
         }
+
+        public int Speed
+        {
+            get {return this.speed;}
+            set {
+                this.speed = value;
+                Console.WriteLine("Changed Speed");
+                
+                if (value >= 100)
+                {
+                    this.speed = 100;
+                    Console.WriteLine("Max Speed !!");
+                }
+
+            }
+        }
+
+        
     }
 
     class Program
